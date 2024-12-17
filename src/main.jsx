905c762d0d9ad8c57e343527a4914d8cf9187eed
Tr-May <may.passionateteacher@gmail.com>
2024-12-17 18:13:10 +0630

    add and remove google fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "./index.css";
 
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
